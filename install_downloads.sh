cbf47d175316151e80bab03a8d7f4577cec563b2
RehabMan <racerrehabman@gmail.com>
2017-09-14 19:45:18 -0700

    fix mount_efi.sh for APFS on 10.13; prepare for 10.13; cleanup

--- install_downloads.sh
+++ install_downloads.sh
@@ -163,18 +163,12 @@ if [ $? -ne 0 ]; then
 fi
 
 # install (injector) kexts in the repo itself
+
+# patching AppleHDA
+HDA=ALC283
+./patch_hda.sh "$HDA"
 install_kext AppleHDA_ALC283.kext
 
-#if [[ 0 == 1 && $MINOR_VER -ge 11 ]]; then
-    #install_kext USBXHC_y50.kext
-    # create custom AppleBacklightInjector.kext and install
-    #./patch_backlight.sh
-    #install_kext AppleBacklightInjector.kext
-    # remove ACPIBacklight.kext if it is installed (doesn't work with 10.11)
-    #if [ -d $SLE/ACPIBacklight.kext ]; then
-    #    $SUDO rm -Rf $SLE/ACPIBacklight.kext
-    #fi
-#fi
 # USBXHC_Envy.kext is not used any more (using USBInjectAll.kext instead)
 $SUDO rm -Rf $SLE/USBXHC_y50.kext $KEXTDEST/USBXHC_Envy.kext
 
